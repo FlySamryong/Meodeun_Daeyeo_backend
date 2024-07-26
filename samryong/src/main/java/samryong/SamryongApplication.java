b062092fcc924ecbdbd94282e8945218a26088e3
jeongchanmin <jjw421412@naver.com>
2024-07-26 16:42:59 +0900

    스프링 초기 환경 설정

---
+++ samryong/src/main/java/samryong/SamryongApplication.java
@@ -0,0 +1,13 @@
+package samryong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamryongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamryongApplication.class, args);
+	}
+
+}
---
+++ samryong/src/test/java/samryong/samryong/SamryongApplicationTests.java
@@ -0,0 +1,13 @@
+package samryong.samryong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamryongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
