648acdd2a1a39c6b645b7962e6c1f186338ff113
jeongchanmin <jjw421412@naver.com>
2024-07-30 21:19:30 +0900

    :recycle: [Refactor] import 순서 리팩토링

--- src/main/java/samryong/SamryongApplication.java
+++ src/main/java/samryong/SamryongApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SamryongApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SamryongApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SamryongApplication.class, args);
+    }
 }
