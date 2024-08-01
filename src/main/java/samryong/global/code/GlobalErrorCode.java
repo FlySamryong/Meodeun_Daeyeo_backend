811e1a0ee25ce743f82590bc659501d084b17697
jeongchanmin <jjw421412@naver.com>
2024-08-01 21:27:34 +0900

    :sparkles: [Feat] API 응답 코드 추가

---
+++ src/main/java/samryong/global/code/GlobalErrorCode.java
@@ -0,0 +1,23 @@
+package samryong.global.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GlobalErrorCode {
+
+    // client 측 오류로 인한 에러(400번대 에러), 서버 측 오류로 인한 에러(500번대 에러)
+
+    // 일반적인 경우의 에러(common)
+    _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+    _NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404", "요청한 데이터를 찾을 수 없습니다"),
+    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
