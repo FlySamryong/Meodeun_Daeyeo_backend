39e07d8e0702bd97c469ab0defd8c9f2f47e22c9
jeongchanmin <jjw421412@naver.com>
2024-08-02 17:40:03 +0900

    :sparkles: [Feat] 스케줄링 환경설정 추가

--- src/main/java/samryong/SamryongApplication.java
+++ src/main/java/samryong/SamryongApplication.java
@@ -2,8 +2,10 @@ package samryong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SamryongApplication {
 
     public static void main(String[] args) {
