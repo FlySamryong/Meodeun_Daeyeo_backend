b1a99a2fbd3bb2756260fa43356937685bc33341
jeongchanmin <jjw421412@naver.com>
2024-09-04 23:05:51 +0900

    :sparkles: [Feat] 인증 사용자 정보 추출 커스텀 어노테이션 작동 resolver 정의

---
+++ src/main/java/samryong/security/resolver/AuthMemberArgumentResolver.java
@@ -0,0 +1,59 @@
+package samryong.security.resolver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import samryong.global.code.GlobalErrorCode;
+import samryong.global.exception.GlobalException;
+import samryong.member.domain.Member;
+import samryong.member.service.MemberService;
+import samryong.security.resolver.annotation.AuthMember;
+
+@Component
+@RequiredArgsConstructor
+public class AuthMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberService memberService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthMember.class)
+                && parameter.getParameterType().equals(Member.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory) {
+
+        Object principal = null;
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            if (authentication.getName().equals("anonymousUser")) {
+                throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            } else {
+                principal = authentication.getPrincipal();
+            }
+        }
+
+        if (principal == null || principal.getClass() == String.class) {
+            throw new GlobalException(GlobalErrorCode.MEMBER_NOT_FOUND);
+        }
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                (UsernamePasswordAuthenticationToken) authentication;
+
+        return memberService.getMember(Long.valueOf(usernamePasswordAuthenticationToken.getName()));
+    }
+}
