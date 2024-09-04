7dd3b94302f23d5e5b6ef2aa92fa6e8594300b74
jeongchanmin <jjw421412@naver.com>
2024-09-04 23:06:37 +0900

    :sparkles: [Feat] 정의한 resolver 등록

--- src/main/java/samryong/global/config/WebConfig.java
+++ src/main/java/samryong/global/config/WebConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import samryong.security.resolver.AuthMemberArgumentResolver;
 import samryong.security.resolver.GetTokenArgumentResolver;
 
 @RequiredArgsConstructor
@@ -12,9 +13,11 @@ import samryong.security.resolver.GetTokenArgumentResolver;
 public class WebConfig implements WebMvcConfigurer {
 
     private final GetTokenArgumentResolver getTokenArgumentResolver;
+    private final AuthMemberArgumentResolver authMemberArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(getTokenArgumentResolver);
+        resolvers.add(authMemberArgumentResolver);
     }
 }
