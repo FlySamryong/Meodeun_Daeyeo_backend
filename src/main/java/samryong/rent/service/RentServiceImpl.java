367ac7ef46c55ffaf9e119e5f10b6c5a4ae41946
jeongchanmin <jjw421412@naver.com>
2024-09-11 17:20:22 +0900

    :sparkles: [Feat] Rent Entity, Repository, Service 추가

---
+++ src/main/java/samryong/rent/service/RentServiceImpl.java
@@ -0,0 +1,8 @@
+package samryong.rent.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RentServiceImpl implements RentService {}
