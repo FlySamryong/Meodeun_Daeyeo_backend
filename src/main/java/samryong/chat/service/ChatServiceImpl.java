ed6c33d30565354deafebe949e98d0a4c22950e6
jeongchanmin <jjw421412@naver.com>
2024-09-11 17:21:36 +0900

    :sparkles: [Feat] 채팅 Service, Controller 추가

---
+++ src/main/java/samryong/chat/service/ChatServiceImpl.java
@@ -0,0 +1,8 @@
+package samryong.chat.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatServiceImpl implements ChatService {}
