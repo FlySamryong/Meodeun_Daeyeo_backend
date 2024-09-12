bc8b53a4e861cafe74584239d8e20426d4a461f7
jeongchanmin <jjw421412@naver.com>
2024-09-12 13:58:07 +0900

    :sparkles: [Feat] 농협 api 응답에 대한 정보를 담는 responseDTO 추가

---
+++ src/main/java/samryong/bank/nonghyup/dto/response/CheckOpenFinAccountDirectResponseDTO.java
@@ -0,0 +1,16 @@
+package samryong.bank.nonghyup.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CheckOpenFinAccountDirectResponseDTO { // 핀테크 발급 확인 API 응답 DTO
+
+    @JsonProperty("FinAcno")
+    private String FinAcno; // 핀-어카운트번호
+
+    @JsonProperty("Header")
+    private ResponseHeader Header; // 응답헤더
+}
