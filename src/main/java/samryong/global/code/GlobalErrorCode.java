3cf799f8af27f8833ab58d9f2382ea025027ec08
jeongchanmin <jjw421412@naver.com>
2024-09-24 20:41:22 +0900

    :sparkles: [Feat] 물품 및 위치 에러 코드 추가

--- src/main/java/samryong/global/code/GlobalErrorCode.java
+++ src/main/java/samryong/global/code/GlobalErrorCode.java
@@ -20,6 +20,13 @@ public enum GlobalErrorCode {
     // 사용자 관련 에러
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER400", "해당 사용자를 찾을 수 없습니다."),
 
+    // 상품 관련 에러
+    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "ITEM400", "해당 상품을 찾을 수 없습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY400", "해당 카테고리를 찾을 수 없습니다."),
+
+    // 위치 관련 에러
+    LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "LOCATION400", "해당 위치를 찾을 수 없습니다."),
+
     // 인증 관련 에러
     KAKAO_AUTH_ERROR(HttpStatus.UNAUTHORIZED, "AUTH403", "카카오 인증에 실패했습니다."),
     AUTH_EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH401", "만료된 토큰입니다."),
