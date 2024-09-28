b4f4d7717bb5dbb8197a7162c4ca4a25628d49bb
jeongchanmin <jjw421412@naver.com>
2024-09-28 19:02:35 +0900

    :sparkles: [Feat] ElasticSearch 를 이용한 검색 Api 구현

--- src/main/java/samryong/domain/item/dto/ItemDTO.java
+++ src/main/java/samryong/domain/item/dto/ItemDTO.java
@@ -5,10 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import samryong.domain.item.dto.CategoryDTO.CategoryRequestDTO;
+import samryong.domain.location.dto.LocationDTO.LocationRequestDTO;
+import samryong.domain.location.dto.LocationDTO.LocationResponseDTO;
 
 public class ItemDTO {
 
@@ -37,5 +41,53 @@ public class ItemDTO {
         private Long deposit;
 
         private ArrayList<CategoryRequestDTO> categoryList;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ItemListRequestDTO {
+
+        private String keyword;
+
+        private CategoryRequestDTO category;
+
+        private LocationRequestDTO location;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ItemPreviewResponseDTO {
+
+        // 아이템 전체 조회 시에 대한 미리 보기 응답
+        private Long id;
+
+        private String name;
+
+        private String status;
+
+        private String createdDate;
+
+        private Long fee;
+
+        private Long deposit;
+
+        private String imageUrl; // 이미지 목록 중 첫번째 값
+
+        private LocationResponseDTO location;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ItemPreviewListResponseDTO {
+
+        private int currentElement; // 현재 페이지 아이템 개수
+        private int totalPage; // 전체 페이지
+        private long totalElement; // 전체 아이템 개수
+        private List<ItemPreviewResponseDTO> itemPreviewResponseDTOList;
     }
 }
