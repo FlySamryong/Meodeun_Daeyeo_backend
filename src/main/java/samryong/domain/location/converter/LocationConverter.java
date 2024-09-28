5d067639b7e5c47787d31dd8c35d4df759d5a711
jeongchanmin <jjw421412@naver.com>
2024-09-28 19:00:19 +0900

    :sparkles: [Feat] Document Converter 추가

--- src/main/java/samryong/domain/location/converter/LocationConverter.java
+++ src/main/java/samryong/domain/location/converter/LocationConverter.java
@@ -1,7 +1,9 @@
 package samryong.domain.location.converter;
 
 import org.springframework.stereotype.Component;
+import samryong.domain.location.document.LocationDocument;
 import samryong.domain.location.dto.LocationDTO.LocationRequestDTO;
+import samryong.domain.location.dto.LocationDTO.LocationResponseDTO;
 import samryong.domain.location.entity.Location;
 
 @Component
@@ -12,6 +14,23 @@ public class LocationConverter {
                 .city(requestDTO.getCity())
                 .district(requestDTO.getDistrict())
                 .neighborhood(requestDTO.getNeighborhood())
+                .build();
+    }
+
+    public static LocationDocument toLocationDocument(Location location) {
+        return LocationDocument.builder()
+                .id(location.getId())
+                .city(location.getCity())
+                .district(location.getDistrict())
+                .neighborhood(location.getNeighborhood())
+                .build();
+    }
+
+    public static LocationResponseDTO toLocationResponseDTO(LocationDocument locationdocument) {
+        return LocationResponseDTO.builder()
+                .city(locationdocument.getCity())
+                .district(locationdocument.getDistrict())
+                .neighborhood(locationdocument.getNeighborhood())
                 .build();
     }
 }
