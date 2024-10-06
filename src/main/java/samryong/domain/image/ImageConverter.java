59331dea9d02bca5236e42f86e3e5ba35823ce21
jeongchanmin <jjw421412@naver.com>
2024-10-06 19:45:38 +0900

    :recycle: [Refactor] 카카오 로그인 이미지 받아오도록 수정

--- src/main/java/samryong/domain/image/ImageConverter.java
+++ src/main/java/samryong/domain/image/ImageConverter.java
@@ -2,11 +2,16 @@ package samryong.domain.image;
 
 import org.springframework.stereotype.Component;
 import samryong.domain.item.entity.Item;
+import samryong.domain.member.entity.Member;
 
 @Component
 public class ImageConverter {
 
     public static Image toItemImage(String imageUri, Item item) {
         return Image.builder().imageUri(imageUri).item(item).build();
+    }
+
+    public static Image toProfileImage(String imageUri, Member member) {
+        return Image.builder().imageUri(imageUri).member(member).build();
     }
 }
