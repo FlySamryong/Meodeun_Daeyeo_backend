74838967b538e30892b03f01d2f33bad147839b5
jeongchanmin <jjw421412@naver.com>
2024-10-06 13:31:18 +0900

    :sparkles: [Feat] 채팅방 목록 조회 api 추가 및 채팅 메시지 전송 테스트 api 추가

--- src/main/java/samryong/domain/chat/controller/ChatRoomController.java
+++ src/main/java/samryong/domain/chat/controller/ChatRoomController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import samryong.domain.chat.aspect.annotation.AuthChatMember;
+import samryong.domain.chat.dto.ChatMessageDTO.ChatMessageRequestDTO;
 import samryong.domain.chat.dto.ChatMessageDTO.ChatMessageResponseListDTO;
+import samryong.domain.chat.dto.ChatRoomDTO.ChatRoomListResponseDTO;
 import samryong.domain.chat.dto.ChatRoomDTO.ChatRoomRequestDTO;
 import samryong.domain.chat.service.ChatMessageService;
 import samryong.domain.chat.service.ChatRoomService;
@@ -41,5 +43,18 @@ public class ChatRoomController {
     public ApiResponse<ChatMessageResponseListDTO> getMessageList(
             @RequestParam(value = "roomId") Long roomId) {
         return ApiResponse.onSuccess("채팅방 메시지 조회 성공", chatMessageService.getMessageList(roomId));
+    }
+
+    @Operation(summary = "사용자 채팅방 목록 전체 조회", description = "사용자의 채팅방 목록을 전체 조회합니다.")
+    @GetMapping("/room/list")
+    public ApiResponse<ChatRoomListResponseDTO> getChatRoomList(@AuthMember Member member) {
+        return ApiResponse.onSuccess("사용자 채팅방 목록 조회 성공", chatRoomService.getChatRoomList(member));
+    }
+
+    @Operation(summary = "채팅 메시지 보내기 테스트 api", description = "채팅 메시지를 보냅니다.")
+    @PostMapping("/message")
+    public ApiResponse<String> message(@RequestBody ChatMessageRequestDTO requestDTO) {
+        chatMessageService.publishMessage(requestDTO);
+        return ApiResponse.onSuccess("메시지 전송 성공", "");
     }
 }
