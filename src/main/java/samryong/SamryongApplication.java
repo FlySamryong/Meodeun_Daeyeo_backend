0b25e443ab82537662e4614380adc74e8317128e
jeongchanmin <jjw421412@naver.com>
2024-11-14 23:44:18 +0900

    :bug: [Bug] 타임존 오류 수정

--- src/main/java/samryong/SamryongApplication.java
+++ src/main/java/samryong/SamryongApplication.java
@@ -1,5 +1,7 @@
 package samryong;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,5 +16,11 @@ public class SamryongApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SamryongApplication.class, args);
+    }
+
+    // Timezone 설정
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
