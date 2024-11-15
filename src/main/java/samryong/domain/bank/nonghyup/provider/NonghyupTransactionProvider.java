43ee90f8539d385aaf8d4621434ff9b4b20fe9e7
jeongchanmin <jjw421412@naver.com>
2024-11-15 19:43:28 +0900

    :bug: [Bug] 농협 api 호출 오류 수정

--- src/main/java/samryong/domain/bank/nonghyup/provider/NonghyupTransactionProvider.java
+++ src/main/java/samryong/domain/bank/nonghyup/provider/NonghyupTransactionProvider.java
@@ -183,7 +183,7 @@ public class NonghyupTransactionProvider {
                 "Tsymd", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))); // 전송일자
         header.put("Trtm", LocalDateTime.now().format(DateTimeFormatter.ofPattern("HHmmss"))); // 전송시간
         header.put("Iscd", iscd); // 이용기관코드
-        header.put("FintechApsno", fintechApsno); // 핀테크 앱 일련번호
+        header.put("FintechApsno", "001"); // 핀테크 앱 일련번호
         header.put("ApiSvcCd", apiSvcCd); // API서비스코드
         header.put(
                 "IsTuno",
